c05561e6fddf559b77537465622acf9d863780bd
Yunjungjincw <qwe3776@kakao.com>
2022-12-13 13:38:53 +0900

    hi world 메시지 추가

--- src/javaPlayground/day1/HelloWorld.java
+++ src/javaPlayground/day1/HelloWorld.java
@@ -3,6 +3,6 @@ package javaPlayground.day1;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hellow World");
-		
+		System.out.println("hellow World");
 	}
 }
