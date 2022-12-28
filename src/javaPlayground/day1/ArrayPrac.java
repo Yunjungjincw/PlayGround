c347fecf9f5df987f74c6d3abd5ff43b9da874fc
Yunjungjincw <qwe3776@kakao.com>
2022-12-29 01:59:45 +0900

    플레이그라운드 22_12_29

---
+++ src/javaPlayground/day1/ArrayPrac.java
@@ -0,0 +1,11 @@
+package javaPlayground.day1;
+
+public class ArrayPrac {
+
+	public static void main(String[] args) {
+		int[] result = new int[9];
+		for(int i=2; i<result.length; i++) {
+			
+ 		}
+	}
+}
--- src/javaPlayground/day2/gugudanClass/Gugudan.java
+++ src/javaPlayground/day2/gugudanClass/Gugudan.java
@@ -2,11 +2,12 @@ package javaPlayground.day2.gugudanClass;
 
 public class Gugudan {
 	public static int[] calculator(int num) {
-		int[] result = new int[9];
-		for(int i=0; i<result.length; i++) {
-			result[i] = num *(i+1);
-		}
-		return result;
-		
+		int[] array = new int[9];
+		for(int i=0; i<array.length; i++) {
+			array[i] =+ i+1;
+			System.out.println();
+		}	
+		return array;
 	}
+	
 }
